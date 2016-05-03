63cefd98c7038505dd87aedf9cfa6e6240fa72a7
Erik Andersson <erik.andersson@limab.se>
2016-05-03 13:55:52 +0200

    Added ObjectTunnels

---
+++ ObjectEntanglementLibrary/OELib/ObjectTunnel/ObjectTunnelClient.cs
@@ -0,0 +1,42 @@
+﻿using OELib.PokingConnection;
+using System;
+using System.Net;
+
+namespace OELib.LibraryBase.ObjectTunnel
+{
+    public class ObjectTunnelClient
+    {
+        private PokingClientConnection _client;
+
+        private IPEndPoint _remoteEndpoint;
+
+        public ObjectTunnelClient(IPEndPoint remoteEndpoint)
+        {
+            _remoteEndpoint = remoteEndpoint;
+            _client = new PokingClientConnection(this);
+            _client.Started += (s, e) => { Connected?.Invoke(s, null); };
+            _client.Stopped += (s, e) => { Disconnected?.Invoke(s, null); };
+        }
+
+        public event EventHandler Connected;
+
+        public event EventHandler Disconnected;
+
+        public event EventHandler<object> ObjectReceived;
+
+        public void ReceiveObject(object objectIn)
+        {
+            ObjectReceived?.Invoke(this, objectIn);
+        }
+
+        public void SendObject<T>(T objectOut)
+        {
+            _client.Reactor.CallRemoteMethod("ReceiveObject", objectOut);
+        }
+
+        public void StartConnectionAttempts()
+        {
+            _client.Start(_remoteEndpoint.Address.ToString(), _remoteEndpoint.Port);
+        }
+    }
+}
---
+++ ObjectEntanglementLibrary/OELib/ObjectTunnel/ObjectTunnelServer.cs
@@ -0,0 +1,33 @@
+﻿using OELib.PokingConnection;
+using System;
+
+namespace LOELib.LibraryBase.ObjectTunnel
+{
+    public class ObjectTunnelServer
+    {
+        private int _port;
+        private PokingServer _server;
+
+        public ObjectTunnelServer(int port)
+        {
+            _port = port;
+            _server = new PokingServer(_port, this);
+            _server.Start();
+        }
+
+        public event EventHandler<object> ObjectReceived;
+
+        public void ReceiveObject(object objectIn)
+        {
+            if (ObjectReceived != null) ObjectReceived(this, objectIn);
+        }
+
+        public void SendObject<T>(T objectOut)
+        {
+            _server.Connections.ForEach(c =>
+                {
+                    c.Reactor.CallRemoteMethod("ReceiveObject", objectOut);
+                });
+        }
+    }
+}
