b909dd4ef0b1c55e4126b9a878042a35879094b3
DeanD <angleofascent@gmail.com>
2016-05-31 08:24:27 +0200

    Added first draft of PriorityQueue

---
+++ ObjectEntanglementLibrary/OELib/LibraryBase/PriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OELib.LibraryBase
+{
+    public class PriorityQueue<T> 
+    {
+        private bool _completed;
+        private List<T> _queue = new List<T>();
+        private readonly object _lock = new object();
+        
+        public void Add(T item)
+        {
+            lock(_lock)
+            {
+                _queue.Add(item);
+                Monitor.Pulse(_lock);
+            }
+        }
+
+        public void CompleteAdding()
+        {
+            lock(_lock)
+            {
+                _completed = true;
+                Monitor.PulseAll(_lock);
+            }
+        }
+
+
+        public T Take()
+        {
+            T item;
+            lock(_lock)
+            {
+                while (_queue.Count == 0 || !_completed)
+                    Monitor.Wait(_lock);
+                if (_completed) throw new ThreadInterruptedException("Priority queue completed.");
+                item = _queue[0];
+                _queue.RemoveAt(0);
+            }
+            return item;
+        }
+        
+    }
+}
