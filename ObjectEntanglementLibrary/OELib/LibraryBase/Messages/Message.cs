953f52abcb769a291bbf851daa4d3d0dfd087c67
DeanD <angleofascent@gmail.com>
2016-06-01 08:28:36 +0200

    Message defaults to normal priority

--- ObjectEntanglementLibrary/OELib/LibraryBase/Messages/Message.cs
+++ ObjectEntanglementLibrary/OELib/LibraryBase/Messages/Message.cs
@@ -5,6 +5,6 @@ namespace OELib.LibraryBase.Messages
     [Serializable]
     public class Message
     {
-        public Priority Priority { get; set; }
+        public Priority Priority { get; set; } = Priority.Normal;
     }
 }
