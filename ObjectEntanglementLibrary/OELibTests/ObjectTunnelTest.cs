1b3764e7fd9359028afa9faea266723ee0c66d6c
DeanD <angleofascent@gmail.com>
2017-10-24 14:44:19 +0200

    Object tunnel test improvement

--- ObjectEntanglementLibrary/OELibTests/ObjectTunnelTest.cs
+++ ObjectEntanglementLibrary/OELibTests/ObjectTunnelTest.cs
@@ -22,7 +22,7 @@ namespace OELibTests
         [TestMethod]
         public void SendRecieveThroughTunnel()
         {
-            var tp = new TestPayload() {D = 2.2, I = 1, S = "Wave"};
+            var tp1 = new TestPayload() {D = 2.2, I = 1, S = "Wave"};
             var server = new ObjectTunnelServer(1044);
             var client = new ObjectTunnelClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1044));
             var go1 = new AutoResetEvent(false);
@@ -34,19 +34,29 @@ namespace OELibTests
             };
             client.StartConnectionAttempts();
             Assert.IsTrue(go1.WaitOne(100));
+            TestPayload tp2 = null, tp3 = null;
             server.ObjectReceived += (s, o) =>
             {
+                tp2 = o as TestPayload;
                 go2.Set();
             };
             client.ObjectReceived += (s, o) =>
             {
+                tp3 = o as TestPayload;
                 go3.Set();
             };
 
-            client.SendObject(tp);
+            client.SendObject(tp1);
             Assert.IsTrue(go2.WaitOne(100));
-            server.SendObject(tp);
+            Assert.AreEqual(tp1.D, tp2.D, 1e-15);
+            Assert.AreEqual(tp1.I, tp2.I);
+            Assert.AreEqual(tp1.S, tp2.S);
+
+            server.SendObject(tp1);
             Assert.IsTrue(go3.WaitOne(100));
+            Assert.AreEqual(tp1.D, tp3.D, 1e-15);
+            Assert.AreEqual(tp1.I, tp3.I);
+            Assert.AreEqual(tp1.S, tp3.S);
         }
     }
 }
