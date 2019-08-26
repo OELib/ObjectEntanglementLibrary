d4895b59e2a8698b498526d3e30e1ce2980a9478
DeanD <angleofascent@gmail.com>
2019-08-26 07:34:35 +0200

    Reactor connection timing test

---
+++ ObjectEntanglementLibrary/OELibTests/UCTests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OELib.UniversalConnection;
+
+namespace OELibTests
+{
+    /// <summary>
+    /// Universal connection tests
+    /// </summary>
+    [TestClass]
+    public class UCTests
+    {
+        private const string _serverDir = @".\TestDirServer\";
+        private const string _clientDir = @".\TestDirClient\";
+        private const int _port = 12345;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Directory.CreateDirectory(_serverDir);
+            Directory.CreateDirectory(_clientDir);
+
+            emptyDir();
+        }
+
+        private static void emptyDir()
+        {
+            var di = new DirectoryInfo(_serverDir);
+            foreach (var file in di.GetFiles()) file.Delete();
+            di = new DirectoryInfo(_clientDir);
+            foreach (var file in di.GetFiles()) file.Delete();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(_serverDir, true);
+            Directory.Delete(_clientDir, true);
+        }
+
+        public class ServerReactor
+        {
+            public void TestMethod()
+            {
+            }
+        }
+
+        public class ClientReactor
+        {
+            public void TestMethod()
+            {
+            }
+        }
+
+
+        /// <summary>
+        /// Tests that the Started event is called after the reactor has been started
+        /// </summary>
+        [TestMethod]
+        public void ReactorConnectionTimingTest()
+        {
+            var sReactor = new ServerReactor();
+            var cReactor = new ClientReactor();
+            var server = new UcServer(_port, sReactor, _serverDir);
+            server.Start();
+            var client = new UCClientConnection(cReactor, _clientDir);
+            var ok = new AutoResetEvent(false);
+            client.Started += (_, __) => { client.Reactor.CallRemoteMethod("TestMethod");
+                ok.Set();
+            };
+            client.Start("127.0.0.1", _port);
+            Assert.IsTrue(ok.WaitOne(50));
+        }
+    }
+}
